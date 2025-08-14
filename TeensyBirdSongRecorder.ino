6d63a1f6423f1c68a39897cb33d11abe336c527c
Quentin McDonald <dqmcdonald@gmail.com>
2025-08-14 18:33:40 +1200

    Fixed hibernation
    FIxed hibernation by turning off I2S before sleeping. This is the first version ready for field testing.

--- TeensyBirdSongRecorder.ino
+++ TeensyBirdSongRecorder.ino
@@ -1,26 +1,26 @@
 
 /*
   A bird song recorder. Record environmental sounds via microphone attached to LineIn of Teensy Audio Shield
-  Files are recorded in 30 minute periods, with a date stamp making up the name of the file.
-  Tested on Teensy 3.6 with Audio shield. MEMS mincrophone with separate 2xAA powersupply.
+  Files are recorded in 5 minute periods, with a date stamp making up the name of the file.
+  Tested on Teensy 3.6 with Audio shield. MEMS mincrophone with separate Lithirm battery powersupply.
   This is based strongly on the recorder example file associated with the Audio Shield Examples
 
   D. Q. McDonald
   July 2018
 
-  
+Updated July 2025 for direct MEMS microphone and not using AudioShield
 
 */
 
+#include <Snooze.h>
 #include <SPI.h>
 #include <SD.h>
-//#include <SD_t3.h>
 #include <SerialFlash.h>
-
 #include <Audio.h>
 #include <Wire.h>
 #include <TimeLib.h>
 #include <String.h>
+
 
 //Variables used to write Wave file
 unsigned long ChunkSize = 0L;
@@ -29,7 +29,7 @@ unsigned int AudioFormat = 1;
 unsigned int numChannels = 1;
 unsigned long sampleRate = 44100;
 unsigned int bitsPerSample = 16;
-unsigned long byteRate = sampleRate * numChannels * (bitsPerSample / 8); // samplerate x channels x (bitspersample / 8)
+unsigned long byteRate = sampleRate * numChannels * (bitsPerSample / 8);  // samplerate x channels x (bitspersample / 8)
 unsigned int blockAlign = numChannels * bitsPerSample / 8;
 unsigned long Subchunk2Size = 0L;
 unsigned long recByteSaved = 0L;
@@ -41,65 +41,75 @@ String filename;
 int led_val = 0;
 int led_inc = 2;
 elapsedMillis led_timer;
-const int LED_UPDATE_PERIOD = 10; // Update LED 50x second
+const int LED_UPDATE_PERIOD = 10;  // Update LED 50x second
 
 
 
-void flashLED( int numflash, int on_time, int off_time );
+void flashLED(int numflash, int on_time, int off_time);
 
 
 
 // Uncomment following line for debugging output
 #define DEBUG 1
 
+
+// Definitions of Audio Library objects
 const int myInput = AUDIO_INPUT_LINEIN;
+AudioPlaySdWav audioSD;
+AudioInputI2S audioInput;
+AudioOutputI2S audioOutput;
+AudioRecordQueue queue1;
 
 
-AudioPlaySdWav           audioSD;
-AudioInputI2S            audioInput;
-AudioOutputI2S           audioOutput;
-AudioRecordQueue         queue1;
 
-//recod from mic
-AudioConnection          patchCord1(audioInput, 0, queue1, 0);
-AudioConnection          patchCord2(audioSD, 0, audioOutput, 0);
-AudioConnection          patchCord3(audioSD, 0, audioOutput, 1);
+//record from mic to buffer:
+AudioConnection patchCord1(audioInput, 0, queue1, 0);
+AudioConnection patchCord2(audioSD, 0, audioOutput, 0);
+AudioConnection patchCord3(audioSD, 0, audioOutput, 1);
 
-//AudioControlSGTL5000     audioShield;
 
-int mode = 0;  // 0=stopped, 1=recording
+
+
+enum rec_modes { STOPPED,
+                 RECORDING };  // Represent state of recording.
+rec_modes mode = STOPPED;
+
 File frec;
-elapsedMillis  msecs;
 
-const long int RECORDING_MINUTES = 1;   // Time for each file in minutes
-const long int RECORDING_MILLIS = 1000 * 60 *  RECORDING_MINUTES; // Time for each file in milliseconds
 
-#define SDCARD_CS_PIN    BUILTIN_SDCARD
-#define SDCARD_MOSI_PIN  11  // not actually used
-#define SDCARD_SCK_PIN   13  // not actually used
+const long int SUNRISE_OFFSET = 5;                                // Number of minutes less than a full day we will sleep for
+const long int RECORDING_MINUTES = 5;                             // Time for each file in minutes
+const long int RECORDING_MILLIS = 1000 * 60 * RECORDING_MINUTES;  // Time for each file in milliseconds
+
+const long int DAY_SLEEP_SECONDS = (60 * 60 * 24) - 240;  // 24 hours - 4 minutes in case sunrise is earlier
+
+#define SDCARD_CS_PIN BUILTIN_SDCARD
+#define SDCARD_MOSI_PIN 11  // not actually used
+#define SDCARD_SCK_PIN 13   // not actually used
 
 #define LED_PIN A16
 
+const int OFFSET = 0;  // Time +/- from sunrise data to begin recording
+
 elapsedMillis etime;
+
+SnoozeAlarm alarm;
+SnoozeAudio audio;
+
+SnoozeBlock snooze_config(alarm, audio);
+
 
 void setup() {
 
-  delay(1000);
-
   pinMode(LED_PIN, OUTPUT);
-
 
 #ifdef DEBUG
   Serial.begin(9600);
+  Serial.print("\n");
 #endif
-  AudioMemory(60);
-  //audioShield.enable();
-  //audioShield.inputSelect(myInput);
-  //audioShield.micGain(40);  //0-63
-  //audioShield.volume(0.5);  //0-1
 
-  flashLED(5, 500, 200 );
-  delay(500);
+  AudioMemory(60);
+  
 
   SPI.setMOSI(SDCARD_MOSI_PIN);
   SPI.setSCK(SDCARD_SCK_PIN);
@@ -113,11 +123,13 @@ void setup() {
     }
   }
 
-  flashLED(5, 200, 500);
-  delay(500);
+  
 #ifdef DEBUG
   Serial.println("SD Card Setup Complete");
 #endif
+
+
+
   // set the Time library to use Teensy 3.0's RTC to keep time
   setSyncProvider(getTeensy3Time);
 #ifdef DEBUG
@@ -130,54 +142,92 @@ void setup() {
 
   Serial.println("RTC setup complete, time and date follows");
   digitalClockDisplay();
-  Serial.println("Initialization done");
+  Serial.println("Initialization done\n");
 #endif
 
-  delay(500);
-  flashLED(5, 500, 500);
+
+  alarm.setRtcTimer(0, 1, 0);  // hour, min, sec
+
+  flashLED(3, 100, 50);
+  delay(100);
 }
 
 
 void loop() {
 
-  if ( led_timer > LED_UPDATE_PERIOD ) {
-    led_val += led_inc;
-    if( led_val > 255 ) {
-      led_val = 255;
-      led_inc = -led_inc;
+
+  if (mode == RECORDING) {
+
+    if (led_timer > LED_UPDATE_PERIOD) {
+      led_val += led_inc;
+      if (led_val > 255) {
+        led_val = 255;
+        led_inc = -led_inc;
+      }
+
+      if (led_val < 0) {
+        led_val = 0;
+        led_inc = -led_inc;
+      }
+      analogWrite(LED_PIN, led_val);
+      led_timer = 0;
     }
 
-    if( led_val < 0 ) {
-      led_val = 0;
-      led_inc = -led_inc;
-    }
-    analogWrite(LED_PIN, led_val );
-    led_timer = 0;
-    
-
+    analogWrite(LED_PIN, led_val);
   }
 
-  analogWrite( LED_PIN, led_val );
+  if (mode == STOPPED) {
 
 
-  if ( mode == 0 ) {
-    startRecording();
+    // Check if it's time to start recording:
+    if (checkForSunrise(OFFSET)) {
+      startRecording();
 #ifdef DEBUG
-    Serial.println("Started Recording");
+      Serial.println("Started Recording");
 #endif
-    mode = 1;
+      etime = 0;
+      mode = RECORDING;
+    } else {
+      // Sleep for another minute and check again:
+#ifdef DEBUG
+      Serial.println("About to sleep for 1 min");
+      delay(1000);
+#endif
+      alarm.setRtcTimer(0, 1, 0);       // hour, min, sec
+      SIM_SCGC6 &= ~SIM_SCGC6_I2S;      // Turn off i2s otherwise it wakes immediately
+      Snooze.hibernate(snooze_config);  // return module that woke processor
+      doReboot();
+    }
   }
 
-  if ( mode == 1 && (etime > RECORDING_MILLIS)) {
+  if (mode == RECORDING && (etime > RECORDING_MILLIS)) {
+    // Recording time has finished:
     stopRecording();
-    mode = 0;
-    etime = 0;
+    mode = STOPPED;
+
+    // Sleep for another nearly a day
+    long int minutes = 60 - SUNRISE_OFFSET - RECORDING_MINUTES;
+
+#ifdef DEBUG
+    Serial.print("About to sleep for 23 hours and ");
+    Serial.print(minutes);
+    Serial.print(" minutes");
+    delay(1000);
+#endif
+
+    alarm.setRtcTimer(23, minutes, 0);  // hour, min, sec
+    SIM_SCGC6 &= ~SIM_SCGC6_I2S;        // Turn off i2s otherwise it wakes immediately
+    Snooze.hibernate(snooze_config);    // return module that woke processor
+    doReboot();
   }
 
-  if (mode == 1) {
+  if (mode == RECORDING) {
     continueRecording();
   }
+}
 
+void doReboot() {
+  SCB_AIRCR = 0x05FA0004;
 }
 
 void startRecording() {
@@ -221,7 +271,7 @@ void stopRecording() {
   Serial.println("stopRecording");
 #endif
   queue1.end();
-  if (mode == 1) {
+  if (mode == RECORDING) {
     while (queue1.available() > 0) {
       frec.write((byte*)queue1.readBuffer(), 256);
       queue1.freeBuffer();
@@ -231,10 +281,9 @@ void stopRecording() {
     flashLED(5, 500, 500);
     frec.close();
   }
-
 }
 
-void writeOutHeader() { // update WAV header with final filesize/datasize
+void writeOutHeader() {  // update WAV header with final filesize/datasize
 
   Subchunk2Size = recByteSaved;
   ChunkSize = Subchunk2Size + 36;
@@ -244,42 +293,61 @@ void writeOutHeader() { // update WAV header with final filesize/datasize
   byte2 = (ChunkSize >> 8) & 0xff;
   byte3 = (ChunkSize >> 16) & 0xff;
   byte4 = (ChunkSize >> 24) & 0xff;
-  frec.write(byte1);  frec.write(byte2);  frec.write(byte3);  frec.write(byte4);
+  frec.write(byte1);
+  frec.write(byte2);
+  frec.write(byte3);
+  frec.write(byte4);
   frec.write("WAVE");
   frec.write("fmt ");
   byte1 = Subchunk1Size & 0xff;
   byte2 = (Subchunk1Size >> 8) & 0xff;
   byte3 = (Subchunk1Size >> 16) & 0xff;
   byte4 = (Subchunk1Size >> 24) & 0xff;
-  frec.write(byte1);  frec.write(byte2);  frec.write(byte3);  frec.write(byte4);
+  frec.write(byte1);
+  frec.write(byte2);
+  frec.write(byte3);
+  frec.write(byte4);
   byte1 = AudioFormat & 0xff;
   byte2 = (AudioFormat >> 8) & 0xff;
-  frec.write(byte1);  frec.write(byte2);
+  frec.write(byte1);
+  frec.write(byte2);
   byte1 = numChannels & 0xff;
   byte2 = (numChannels >> 8) & 0xff;
-  frec.write(byte1);  frec.write(byte2);
+  frec.write(byte1);
+  frec.write(byte2);
   byte1 = sampleRate & 0xff;
   byte2 = (sampleRate >> 8) & 0xff;
   byte3 = (sampleRate >> 16) & 0xff;
   byte4 = (sampleRate >> 24) & 0xff;
-  frec.write(byte1);  frec.write(byte2);  frec.write(byte3);  frec.write(byte4);
+  frec.write(byte1);
+  frec.write(byte2);
+  frec.write(byte3);
+  frec.write(byte4);
   byte1 = byteRate & 0xff;
   byte2 = (byteRate >> 8) & 0xff;
   byte3 = (byteRate >> 16) & 0xff;
   byte4 = (byteRate >> 24) & 0xff;
-  frec.write(byte1);  frec.write(byte2);  frec.write(byte3);  frec.write(byte4);
+  frec.write(byte1);
+  frec.write(byte2);
+  frec.write(byte3);
+  frec.write(byte4);
   byte1 = blockAlign & 0xff;
   byte2 = (blockAlign >> 8) & 0xff;
-  frec.write(byte1);  frec.write(byte2);
+  frec.write(byte1);
+  frec.write(byte2);
   byte1 = bitsPerSample & 0xff;
   byte2 = (bitsPerSample >> 8) & 0xff;
-  frec.write(byte1);  frec.write(byte2);
+  frec.write(byte1);
+  frec.write(byte2);
   frec.write("data");
   byte1 = Subchunk2Size & 0xff;
   byte2 = (Subchunk2Size >> 8) & 0xff;
   byte3 = (Subchunk2Size >> 16) & 0xff;
   byte4 = (Subchunk2Size >> 24) & 0xff;
-  frec.write(byte1);  frec.write(byte2);  frec.write(byte3);  frec.write(byte4);
+  frec.write(byte1);
+  frec.write(byte2);
+  frec.write(byte3);
+  frec.write(byte4);
   frec.close();
 #ifdef DEBUG
   Serial.println("header written");
@@ -302,8 +370,7 @@ void digitalClockDisplay() {
   Serial.println();
 }
 
-time_t getTeensy3Time()
-{
+time_t getTeensy3Time() {
   return Teensy3Clock.get();
 }
 
@@ -318,19 +385,19 @@ void printDigits(int digits) {
 String getFileName() {
   // Returns a filename based on the current date and time: MMDDHHMM
   String ret = "";
-  if ( month() < 10 ) {
+  if (month() < 10) {
     ret += "0";
   }
   ret += String(month());
-  if ( day() < 10) {
+  if (day() < 10) {
     ret += "0";
   }
   ret += String(day());
-  if ( hour() < 10) {
+  if (hour() < 10) {
     ret += "0";
   }
   ret += String(hour());
-  if ( minute() < 10) {
+  if (minute() < 10) {
     ret += "0";
   }
   ret += String(minute());
@@ -338,14 +405,13 @@ String getFileName() {
   return ret;
 }
 
-void flashLED( int numflash, int on_time, int off_time ) {
+void flashLED(int numflash, int on_time, int off_time) {
   // Flash the builtin LED numflash times with on_time and off_time between each one
   int i;
-  for ( i = 0; i < numflash; i++) {
+  for (i = 0; i < numflash; i++) {
     digitalWrite(LED_PIN, HIGH);
     delay(on_time);
     digitalWrite(LED_PIN, LOW);
     delay(off_time);
   }
 }
-
